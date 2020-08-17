e28fca1462ae5565dd6de752485a53ba9e54cdbf
Eric Hocking <ehock7229@outlook.com>
2020-08-17 10:01:22 -0400

    load SecurityBundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,5 +6,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    TheHostingTool\CoreBundle\CoreBundle::class => ['all' => true],
+    TheHostingTool\Bundle\CoreBundle\CoreBundle::class => ['all' => true],
+    TheHostingTool\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
